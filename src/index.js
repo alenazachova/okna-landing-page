41f6a54f327b596d1a24c347f2a4a9d30e7bb7d1
alenazachova <ala.zachova@gmail.com>
2023-08-07 14:37:22 +0200

    tag manager + script?

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    //the event occurred
-    console.log("Amadeus - okna");
+document.addEventListener("DOMContentLoaded", (event) => {
+  //the event occurred
+  console.log("Amadeus - okna");
 
-    document.querySelector(".intro__mouse").addEventListener("click", (e) => {
+  document.querySelector(".intro__mouse").addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = document.querySelector(".intro__mouse").getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
 
-        e.preventDefault();
-        const href = document.querySelector(".intro__mouse").getAttribute("href");
-        const offsetTop = document.querySelector(href).offsetTop;
-
-        scroll({
-            top: offsetTop,
-            behavior: "smooth"
-        });
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
     });
-})
+  });
+});
+
+//  const number = document.querySelector("#number");
+
+function getSelectionText() {
+  const email = document.querySelector("#email");
+  if (window.getSelection) {
+    email = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    email = document.selection.createRange().email;
+  }
+  return email;
+}
+
+document.addEventListener("copy", function (e) {
+  dataLayer.push({
+    event: "textCopied",
+    clipboardText: getSelectionText(),
+    clipboardLength: getSelectionText().length,
+  });
+});
